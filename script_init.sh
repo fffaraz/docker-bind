84198a90e9d5d831fb096f50e2097a863f670c5e
Faraz Fallahi <fffaraz@gmail.com>
2017-07-17 18:26:33 -0500

    Create script_init.sh

---
+++ script_init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euxo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yq update < /dev/null
+apt-get -yq upgrade < /dev/null
+apt-get -yq install bind9 bind9utils bind9-host < /dev/null
+
+mkdir -m 0775 -p /var/run/named
+chown root:bind /var/run/named
+
+mkdir -m 0775 -p /var/cache/bind
+chown root:bind /var/cache/bind
+
+rm -rf /var/lib/apt/lists/*
+
+cat > /etc/bind/named.conf <<'EOL'
+include "/etc/bind/named.conf.options";
+include "/etc/bind/named.conf.local";
+EOL
+
+cat > /etc/bind/named.conf.options <<'EOL'
+options {
+	directory "/var/cache/bind";
+	dnssec-validation auto;
+	auth-nxdomain no; # conform to RFC1035
+	listen-on-v6 { any; };
+	recursion no;
+	allow-query { any; };
+	allow-transfer { none; };
+};
+EOL
+
+cat > /etc/bind/named.conf.local <<'EOL'
+zone "." {
+	type master;
+	file "/data/root.zone";
+};
+EOL
