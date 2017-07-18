df1b353770b251697f624648f044c1d6fd085edb
Faraz Fallahi <fffaraz@gmail.com>
2017-07-17 19:06:22 -0500

    Update script_run.sh

--- script_run.sh
+++ script_run.sh
@@ -6,12 +6,13 @@ mkdir -p /data
 [ ! -f /data/zone.conf ] && touch /data/zone.conf
 
 [ ! -f /data/root.zone ] && cat > /data/root.zone <<'EOL'
+$TTL 600
 @ IN SOA . dnsadmin.example.net. (
- 2009092001 ; Serial
+ 2017010101 ; Serial
  604800     ; Refresh
  86400      ; Retry
  1206900    ; Expire
- 300 )      ; Negative Cache TTL
+ 600 )      ; Negative Cache TTL
    IN NS ns1.example.net.
 *. IN A  127.0.0.1
 EOL
