997be5c349a231ff80a02f96ca56b8c826faac79
Faraz Fallahi <fffaraz@gmail.com>
2022-04-24 12:23:54 -0400

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -15,7 +15,6 @@ chown root:bind /var/cache/bind
 cat > /etc/bind/named.conf <<'EOL'
 include "/etc/bind/named.conf.options";
 include "/conf/zones.conf";
-
 EOL
 
 cat > /etc/bind/named.conf.options <<'EOL'
